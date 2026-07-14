7e815d3cbfb4f303b2d2656b07449c2043641f1e
Serhii <49346078+QuantPixel@users.noreply.github.com>
2026-07-14 19:03:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-﻿const CACHE_NAME = 'ble-app-v1';
+﻿const CACHE_NAME = 'ble-app-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Установка Service Worker и кэширование файлов
